2bfea1a27a2b4c6b99a0b8bc5ad9ac861999e4d8
void <git.email.address@googlemail.com>
2024-06-23 03:17:09 +0200

    Implemented zstd compression

--- src/csv_parser/main.rs
+++ src/csv_parser/main.rs
@@ -1,5 +1,5 @@
 use std::{
-	arch::x86_64, collections::BTreeMap, env, fs, path::PathBuf
+	collections::BTreeMap, env, fs::{self, File}, io::Write, path::{Path, PathBuf}
 };
 use serde;
 use rkyv::{
@@ -29,7 +29,7 @@ struct CsvRecord<'a> {
 	open_interest: &'a str
 }
 
-#[derive(Debug, Archive, Serialize, Deserialize)]
+#[derive(Debug, Archive, Serialize, Deserialize, Clone)]
 struct OhlcRecord {
 	symbol: Option<String>,
 	time: NaiveDateTime,
@@ -49,29 +49,30 @@ struct OhlcKey {
 
 fn main() {
 	let arguments: Vec<String> = env::args().collect();
-	if arguments.len() != 2 {
+	if arguments.len() != 3 {
 		println!("Usage:");
 		let application = env::current_exe().unwrap();
-		println!("{} <path to Barchart .csv files>", application.display());
+		println!("{} <path to Barchart .csv files> <output directory>", application.display());
 		return;
 	}
-	let path = PathBuf::from(&arguments[1]);
-	read_ticker_directories(path);
+	let input_directory = PathBuf::from(&arguments[1]);
+	let output_directory = PathBuf::from(&arguments[2]);
+	read_ticker_directories(&input_directory, &output_directory);
 }
 
-fn read_ticker_directories(path: PathBuf) {
+fn read_ticker_directories(input_directory: &PathBuf, output_directory: &PathBuf) {
 	let stopwatch = Stopwatch::start_new();
-	let mut paths: Vec<PathBuf> = [].to_vec();
-	for_each_directory(path, "Unable to read ticker directory", |tickers_path| {
-		for_each_directory(tickers_path, "Unable to read time frames", |time_frame_path| {
+	let mut paths = Vec::new();
+	for_each_directory(input_directory, "Unable to read ticker directory", |tickers_path| {
+		for_each_directory(&tickers_path, "Unable to read time frames", |time_frame_path| {
 			paths.push(time_frame_path);
 		});
 	});
-	paths.par_iter().for_each(process_time_frame_data);
+	paths.par_iter().for_each(|x| process_time_frame_data(x, output_directory));
 	println!("Processed all directories in {} ms", stopwatch.elapsed_ms());
 }
 
-fn for_each_directory(path: PathBuf, error_message: &str, handler: impl FnMut(PathBuf) -> ()) {
+fn for_each_directory(path: &PathBuf, error_message: &str, handler: impl FnMut(PathBuf) -> ()) {
 	fs::read_dir(path)
 		.expect(error_message)
 		.filter(|x| x.is_ok())
@@ -80,7 +81,7 @@ fn for_each_directory(path: PathBuf, error_message: &str, handler: impl FnMut(Pa
 		.for_each(handler);
 }
 
-fn process_time_frame_data(path: &PathBuf) {
+fn process_time_frame_data(path: &PathBuf, output_directory: &PathBuf) {
 	let csv_paths = fs::read_dir(path.clone())
 		.expect("Unable to get list of .csv files")
 		.filter(|x| x.is_ok())
@@ -90,8 +91,8 @@ fn process_time_frame_data(path: &PathBuf) {
 			x.extension().is_some() &&
 			x.extension().unwrap() == "csv"
 		);
-	println!("Processing files in {}", path.to_str().unwrap());
-	let stopwatch = Stopwatch::start_new();
+	println!("Processing files in \"{}\"", path.to_str().unwrap());
+	let mut stopwatch = Stopwatch::start_new();
 	let mut ohlc_map = BTreeMap::new();
 	for csv_path in csv_paths {
 		let mut reader = csv::Reader::from_path(csv_path)
@@ -142,5 +143,37 @@ fn process_time_frame_data(path: &PathBuf) {
 			}
 		}
 	}
-	println!("Merged {} records from {} into B-tree in {} ms", ohlc_map.len(), path.to_str().unwrap(), stopwatch.elapsed_ms());
+	println!("Merged {} records from {} in {} ms", ohlc_map.len(), path.to_str().unwrap(), stopwatch.elapsed_ms());
+	stopwatch.restart();
+	let iter = path.iter();
+	let count = 2;
+	let mut final_tokens = iter.skip(path.components().count() - count).take(count);
+	let mut get_token = || final_tokens.next().unwrap().to_str().unwrap();
+	let symbol = get_token();
+	let time_frame =  get_token();
+	let file_name = format!("{symbol}.{time_frame}");
+	let output_path = Path::new(output_directory).join(file_name);
+	let ohlc_records = ohlc_map.values().cloned().collect::<Vec<OhlcRecord>>();
+	match rkyv::to_bytes::<_, 1024>(&ohlc_records) {
+		Ok(binary_data) => {
+			match File::create(output_path.clone()) {
+				Ok(mut file) => {
+					match zstd::stream::copy_encode(binary_data.as_slice(), file, 1) {
+						Ok(_) => {
+							println!("Serialized records to \"{}\" in {} ms", output_path.to_str().unwrap(), stopwatch.elapsed_ms());
+						}
+						Err(error) => {
+							eprintln!("Failed to write output to file \"{}\": {error}", output_path.to_str().unwrap());
+						}
+					}
+				}
+				Err(error) => {
+					eprintln!("Failed to create output file \"{}\": {error}", output_path.to_str().unwrap());
+				}
+			}
+		}
+		Err(error) => {
+			eprintln!("Failed to serialize B-tree map: {error}");
+		}
+	}
 }
