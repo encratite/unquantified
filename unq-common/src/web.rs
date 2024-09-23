19de69fa34c2a0f99f9d4c5b551720e78ccb4d65
void <git.email.address@googlemail.com>
2024-09-23 09:46:21 +0200

    Fixed the rounding issue in a questionable way

--- unq-common/src/web.rs
+++ unq-common/src/web.rs
@@ -1,6 +1,6 @@
-use serde::{Deserialize, Serialize, Serializer};
+use serde::{Deserialize, Deserializer, Serialize, Serializer};
 
-#[derive(Clone, Debug, Deserialize)]
+#[derive(Clone, Debug)]
 pub struct WebF64 {
 	value: f64,
 	precision: i32
@@ -34,5 +34,19 @@ impl Serialize for WebF64 {
 		let factor = 10f64.powi(self.precision);
 		let rounded = (self.value * factor).round() / factor;
 		serializer.serialize_f64(rounded)
+	}
+}
+
+impl<'de> Deserialize<'de> for WebF64 {
+	fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
+	where
+		D: Deserializer<'de>,
+	{
+		let value = f64::deserialize(deserializer)?;
+		let output = WebF64 {
+			value,
+			precision: 2
+		};
+		Ok(output)
 	}
 }
