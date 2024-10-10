be40eebc5979907564491a9de882f33369879cfc
void <git.email.address@googlemail.com>
2024-10-10 22:49:14 +0200

    Removed the Rhai DSL gimmick

--- unq-strategy/src/strategy/script.rs
+++ unq-strategy/src/strategy/script.rs
@@ -1,18 +1,17 @@
 use std::cell::RefCell;
-use std::path::{Path, PathBuf};
+use std::path::Path;
 use std::rc::Rc;
 use anyhow::{Result, bail, anyhow, Context};
 use regex::Regex;
-use rhai::{Engine, Expr, Scope, Stmt, AST};
+use rhai::{Engine, Scope, AST};
 use unq_common::backtest::Backtest;
 use unq_common::strategy::{Strategy, StrategyParameter, StrategyParameterType, StrategyParameters};
 use crate::CONTRACTS_PARAMETER;
 
-const DECLARE_STATEMENT: &'static str = "declare";
 const API_CONSTANT: &'static str = "api";
 const POSITIONS_PARAMETER: &'static str = "positions";
 
-type ApiContextCell<'a> = Rc<RefCell<ApiContext>>;
+type ApiContextCell = Rc<RefCell<ApiContext>>;
 
 #[derive(Clone)]
 enum PositionSizing {
@@ -30,16 +29,11 @@ enum TradeSignal {
 
 pub struct ScriptStrategy<'a> {
 	symbols: Vec<String>,
-	context: ApiContextCell<'a>,
-	script: CompiledScript<'a>,
-	backtest: RefCell<Backtest>
-}
-
-struct CompiledScript<'a> {
+	context: ApiContextCell,
 	engine: Engine,
 	scope: Scope<'a>,
-	script_ast: AST,
-	declared_variables: Vec<String>
+	script: AST,
+	backtest: RefCell<Backtest>
 }
 
 #[derive(Clone)]
@@ -61,7 +55,9 @@ impl<'a> ScriptStrategy<'a> {
 		let file_name = format!("{script}.rhai");
 		let path = Path::new(script_directory).join(&file_name);
 		let mut scope: Scope<'a> = scope.clone();
-		let script = Self::compile_script(path, &mut scope)?;
+		let engine = Engine::new();
+		let script = engine.compile_file(path)
+			.map_err(|error| anyhow!("Failed to compile script: {error}"))?;
 		let current_symbol = symbols.first()
 			.with_context(|| "No symbols specified")?
 			.clone();
@@ -80,6 +76,8 @@ impl<'a> ScriptStrategy<'a> {
 		let mut strategy = Self {
 			symbols: symbols.clone(),
 			context: context_cell,
+			engine,
+			scope,
 			script,
 			backtest
 		};
@@ -92,7 +90,7 @@ impl<'a> ScriptStrategy<'a> {
 		for parameter in parameters.iter() {
 			let name = parameter.name.as_str();
 			if name != POSITIONS_PARAMETER && name != CONTRACTS_PARAMETER {
-				Self::process_declared_variable(parameter, &mut scope)?;
+				Self::add_parameter_to_scope(parameter, &mut scope)?;
 			}
 		}
 		let positions_parameter = parameters.get_string(POSITIONS_PARAMETER)?;
@@ -118,7 +116,7 @@ impl<'a> ScriptStrategy<'a> {
 		Self::new(script, script_directory, symbols, position_sizing, contracts, &scope, backtest)
 	}
 
-	fn process_declared_variable(parameter: &StrategyParameter, scope: &mut Scope) -> Result<()> {
+	fn add_parameter_to_scope(parameter: &StrategyParameter, scope: &mut Scope) -> Result<()> {
 		match parameter.get_type()? {
 			StrategyParameterType::NumericSingle => {
 				if let Some(value) = parameter.value.clone() {
@@ -153,72 +151,9 @@ impl<'a> ScriptStrategy<'a> {
 		Ok(())
 	}
 
-	fn compile_script(path: PathBuf, scope: &mut Scope<'a>) -> Result<CompiledScript<'a>> {
-		let engine = Engine::new();
-		let original_ast = engine.compile_file(path)
-			.map_err(|error| anyhow!("Failed to compile script: {error}"))?;
-		let mut declared_variables = Vec::new();
-		/*
-		The following code scans for special external variable declaration statements that look like this:
-
-		declare(int_variable, 123);
-		declare(float_variable, 45.6);
-		declare(string_variable, "Some string.");
-
-		These statements are removed from the AST prior to execution and create an explicit mapping from web UI parameters to the scope in the script.
-		The first argument is the name of the variable while the second one is the default value that will be used if the user didn't specify it in the UI.
-		*/
-		for statement in original_ast.statements().to_vec() {
-			if let Stmt::FnCall(function, _) = statement {
-				if function.name == DECLARE_STATEMENT {
-					let arguments = &function.args;
-					let name_argument = &arguments[0];
-					let value_argument = arguments[1].clone();
-					let Expr::Variable(variable_data, _, _) = name_argument else {
-						bail!("Invalid first argument in declare statement, expected a variable");
-					};
-					let (_, ref variable_name, _, _) = **variable_data;
-					declared_variables.push(variable_name.to_string());
-					match value_argument {
-						Expr::IntegerConstant(value, _) => {
-							scope.push(variable_name, value);
-						},
-						Expr::FloatConstant(value, _) => {
-							scope.push(variable_name, value);
-						},
-						Expr::StringConstant(value, _) => {
-							scope.push(variable_name, value);
-						},
-						_ => bail!("Invalid second argument in declare statement, expected an immediate value")
-					}
-				}
-			}
-		}
-		let filtered_statements: Vec<Stmt> = original_ast.statements()
-			.iter()
-			.cloned()
-			.filter(|statement| {
-				if let Stmt::FnCall(function, _) = statement {
-					function.name != DECLARE_STATEMENT
-				} else {
-					true
-				}
-			})
-			.collect();
-		let module = original_ast.shared_lib().clone();
-		let transformed_ast = AST::new(filtered_statements, module);
-		let script = CompiledScript {
-			engine,
-			scope: scope.clone(),
-			script_ast: transformed_ast,
-			declared_variables
-		};
-		Ok(script)
-	}
-
 	fn register_functions(&mut self) {
-		self.script.scope.push_constant(API_CONSTANT, self.context.clone());
-		let engine = &mut self.script.engine;
+		self.scope.push_constant(API_CONSTANT, self.context.clone());
+		let engine = &mut self.engine;
 		engine.register_type_with_name::<ApiContextCell>(API_CONSTANT);
 		engine.register_fn("buy", |api: &mut ApiContextCell| api.borrow_mut().buy());
 		engine.register_fn("sell", |api: &mut ApiContextCell| api.borrow_mut().sell());
@@ -230,8 +165,7 @@ impl<'a> Strategy for ScriptStrategy<'a> {
 	fn next(&mut self) -> Result<()> {
 		for symbol in self.symbols.iter() {
 			self.context.borrow_mut().current_symbol = symbol.clone();
-			let script = &mut self.script;
-			script.engine.run_ast_with_scope(&mut script.scope, &script.script_ast)
+			self.engine.run_ast_with_scope(&mut self.scope, &self.script)
 				.map_err(|error| anyhow!("Failed to run script: {error}"))?;
 		}
 		todo!()
